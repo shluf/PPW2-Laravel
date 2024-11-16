3abd2f12f62c2a595f875258250220b72cc97dac
shluf <luthfisalis09@gmail.com>
2024-11-17 00:13:34 +0700

    Pertemuan 12 - Store API

--- app/Http/Controllers/Api/BookApiController.php
+++ app/Http/Controllers/Api/BookApiController.php
@@ -14,4 +14,30 @@ class BookApiController extends Controller
 
         return new BookResource(true, 'List Data Buku', $books);
     }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'judul' => 'required|string|max:255',
+            'penulis' => 'required|string|max:255',
+            'harga' => 'required|numeric',
+            'tgl_terbit' => 'required|date',
+            'filename' => 'nullable|string|max:255',
+            'filepath' => 'nullable|string|max:255',
+        ]);
+
+        $book = Buku::create([
+            'judul' => $validated['judul'],
+            'penulis' => $validated['penulis'],
+            'harga' => $validated['harga'],
+            'tgl_terbit' => $validated['tgl_terbit'],
+            'filename' => $validated['filename'] ?? null,
+            'filepath' => $validated['filepath'] ?? null,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Buku berhasil disimpan',
+            'data' => new BookResource(true, 'Berhasil disimpan', $book)
+        ]);
+    }
 }
